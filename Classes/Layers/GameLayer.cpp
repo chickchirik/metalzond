412caaada8d7da8c5e1ff43b2b646d2788dd09c4
chickchirik <orangeboi@yandex.ru>
2020-01-16 15:25:03 +0300

    removed testing code from GameLayer class

--- Classes/Layers/GameLayer.cpp
+++ Classes/Layers/GameLayer.cpp
@@ -22,13 +22,5 @@ bool GameLayer::init() {
     auto director = Director::getInstance();
     auto visibleSize = director->getVisibleSize();
     Vec2 origin = director->getVisibleOrigin();
-
-    auto logoSprite = Sprite::create("chickchirikMobileLogo.jpg");
-    logoSprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-    this->addChild(logoSprite, 0);
-
-    auto testlabel = Label::createWithTTF("TEST LABEL", "fonts/arial.ttf", 36);
-    testlabel->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
-    this->addChild(testlabel, 1);
     return true;
 }
