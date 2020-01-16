e15cba4a6801af2d2b8a1a1d2aaa803655522726
chickchirik <orangeboi@yandex.ru>
2020-01-16 16:12:33 +0300

    refactored code

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -43,22 +43,17 @@ AppDelegate::~AppDelegate() {
 #endif
 }
 
-// if you want a different context, modify the value of glContextAttrs
-// it will affect all platforms
 void AppDelegate::initGLContextAttrs() {
     // set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
     GLView::setGLContextAttrs(glContextAttrs);
 }
 
-// if you want to use the package manager to install more packages,
-// don't modify or remove this function
 static int register_all_packages() {
     return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
@@ -69,22 +64,15 @@ bool AppDelegate::applicationDidFinishLaunching() {
 #endif
         director->setOpenGLView(glview);
     }
-    // turn on display FPS
     director->setDisplayStats(true);
-    // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
-    // Set the design resolution
     glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
     register_all_packages();
-    // create a scene. it's an autorelease object
-    // auto scene = GameScene::createScene();
     auto scene = TitleLayer::createScene();
-    // run
     director->runWithScene(scene);
     return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 #if USE_AUDIO_ENGINE
@@ -92,7 +80,6 @@ void AppDelegate::applicationDidEnterBackground() {
 #endif
 }
 
-// this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
 #if USE_AUDIO_ENGINE
