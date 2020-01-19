b58ec3b8a983cb911d1c207c1d7982fa5dd9b694
chickchirik <orangeboi@yandex.ru>
2020-01-19 12:33:26 +0300

    added player class

---
+++ Classes/Objects/Player.cpp
@@ -0,0 +1,15 @@
+/*
+
+    Player.cpp
+
+    Created by <chickchirik> on 12/01/2020.
+
+    DESCRIPTION:
+
+*/
+
+#include "Player.hpp"
+
+Player::Player()  {}
+
+Player::~Player() {}
---
+++ Classes/Objects/Player.hpp
@@ -0,0 +1,21 @@
+/*
+
+    Player.hpp
+
+    Created by <chickchirik> on 12/01/2020.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class Player {
+public:
+    Player();
+    ~Player();
+};
