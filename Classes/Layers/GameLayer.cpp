866d3181a79ce9b9e670eae7c114e98a17a943af
chickchirik <orangeboi@yandex.ru>
2020-01-21 01:19:23 +0300

    added invisible walls to the game scene

--- Classes/Layers/GameLayer.cpp
+++ Classes/Layers/GameLayer.cpp
@@ -25,7 +25,14 @@ bool GameLayer::init() {
     auto director = Director::getInstance();
     auto visibleSize = director->getVisibleSize();
     Vec2 origin = director->getVisibleOrigin();
-    
+
+//walls
+    auto wall = Node::create();
+    auto wallBody = PhysicsBody::createEdgeBox(visibleSize, PhysicsMaterial(0.1f, 1.0f, 0.0f));
+    wall->setPhysicsBody(wallBody);
+    wall->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    this->addChild(wall);
+
     player = new Player(visibleSize);
     this->addChild(player->getSprite());
     //freeing memoray allocated for the player object
