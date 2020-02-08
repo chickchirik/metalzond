29c48575f04938261fc9d9a57f565665d5927ca3
chickchirik <orangeboi@yandex.ru>
2020-02-08 12:18:31 +0300

    added comments to player class

--- Classes/Objects/Player.cpp
+++ Classes/Objects/Player.cpp
@@ -4,8 +4,6 @@
 
     Created by <chickchirik> on 12/01/2020.
 
-    DESCRIPTION:
-
 */
 
 #include "Player.hpp"
@@ -22,31 +20,33 @@ Player::Player(cocos2d::Size visibleSize, const Vec2& origin)  {
     auto shapeCache = PhysicsShapeCache::getInstance();
     shapeCache->setBodyOnSprite("playerBody", playerSprite);
 
-    for (auto spriteEntry : spriteCache->getSpriteFrames()) {
+    /*  get all sprite frames and set them to player instance   */
+    for (auto spriteEntry : spriteCache->getSpriteFrames()) { /*    invoking custom written sprite frames getter (see CCSpriteFrameCache.h) */
         std::string spriteFileName = spriteEntry.first;
         std::string spriteName = spriteFileName.substr(0, spriteFileName.rfind("."));
         auto newSprite = Sprite::createWithSpriteFrame(spriteEntry.second);
         auto newSpriteSize = newSprite->getContentSize();
+        /*  sprite frames invisible by default */
         newSprite->setVisible(false);
-        if (spriteName.find("Left") != -1) {
+        if (spriteName.find("Left") != -1) {            /*  set jet fire sprite for left jet    */
             newSprite->setPosition(Vec2(newSpriteSize.width / 2.5, -newSpriteSize.height / 5));
             playerSprite->addChild(newSprite, -1, spriteName);
-        } else if (spriteName.find("Right") != -1) {
+        } else if (spriteName.find("Right") != -1) {    /*  set jet fire sprite for right jet   */
             newSprite->setPosition(Vec2(playerSize.width - newSpriteSize.width / 2.5, -newSpriteSize.height / 5));
             playerSprite->addChild(newSprite, -1, spriteName);
-        } else {
+        } else {                                        /*  set faces   */
             newSprite->setPosition(Vec2(playerSize.width / 2, playerSize.height / 2.5));
             newSprite->setScale(1.5);
             playerSprite->addChild(newSprite, 1, spriteName);
         }
     }
-    playerSprite->getChildByName("happyFace")->setVisible(true);
+    playerSprite->getChildByName("happyFace")->setVisible(true); /* start with happy face on    */
 }
 
 Player::~Player() {}
 
 void Player::handleInput(const Vec2& touchLocation) {
-    if (touchLocation == Vec2::ZERO) {
+    if (touchLocation == Vec2::ZERO) {  /*  treat zero touch location as no input   */
         playerSprite->getChildByName("jetFireLeft")->setVisible(false);
         playerSprite->getChildByName("jetFireRight")->setVisible(false);
         return;
@@ -54,23 +54,27 @@ void Player::handleInput(const Vec2& touchLocation) {
     auto visibleSize = Director::getInstance()->getVisibleSize();
     auto playerSize = playerSprite->getContentSize();
     auto body = playerSprite->getPhysicsBody();
-    if (touchLocation.x < visibleSize.width / 2) {
+    if (touchLocation.x < visibleSize.width / 2) {      /*  touching left side of the screen invokes left jet fire impulse      */
         body->applyImpulse(Vec2(100, 1500), Vec2(-playerSize.width, -playerSize.height));
         playerSprite->getChildByName("jetFireLeft")->setVisible(true);
-    } else {
+    } else {                                            /*  touching right side of the screen invokes right jet fire impulse    */
         body->applyImpulse(Vec2(-100, 1500), Vec2(playerSize.width, -playerSize.height));
         playerSprite->getChildByName("jetFireRight")->setVisible(true);
     }
+    /*  give forward impulse bt default     */
     body->applyImpulse(Vec2(-1, 9500));
 }
 
 void Player::processState() {
     std::string prevEmotion = currEmotion;
     currEmotion = "happyFace";
+    /*  checking for scared face    */
     if (health < fullHealth*0.5) { currEmotion = "scaredFace"; }
-    float playerRotation = (-playerSprite->getPhysicsBody()->getRotation() + 90) * M_PI / 180.0;
+    /*  checking for surprised face */
+    float playerRotation = (-playerSprite->getPhysicsBody()->getRotation() + 90) * M_PI / 180.0; /* cocos2d returns degrees, need to convert to radians */
     float sineValue = std::sin(playerRotation);
-    if (sineValue >= -1 && sineValue <= 0) { currEmotion = "surprisedFace"; }
+    if (sineValue >= -1 && sineValue <= 0) { currEmotion = "surprisedFace"; }   /*  if player is upside down - set surprised face   */
+    /*  setting updated face emotion    */
     playerSprite->getChildByName(prevEmotion)->setVisible(false);
     playerSprite->getChildByName(currEmotion)->setVisible(true);
 }
--- Classes/Objects/Player.hpp
+++ Classes/Objects/Player.hpp
@@ -5,7 +5,8 @@
     Created by <chickchirik> on 12/01/2020.
 
     DESCRIPTION:
-
+    metalzond player class used in game scene. It handles player's input
+    and processes player state.
 */
 
 #pragma once
@@ -21,13 +22,14 @@ class Player {
 private:
     std::string currEmotion = "happyFace";
     Sprite* playerSprite = nullptr;
+    /*  temporary health placeholder    */
     uint8_t fullHealth = 100;
     uint8_t health = 100;
-    void handleInput(const Vec2& touchLocation);
-    void processState();
+    void handleInput(const Vec2& touchLocation);    /*  affects on player physics body based on the input and sets fire sprites */
+    void processState();                            /*  checks player current state and sets proper sprite based on it          */
 public:
     Player(cocos2d::Size visibleSize, const Vec2& origin);
     ~Player();
     inline Sprite* getSprite() { return playerSprite; }
-    void update(const Vec2& touchLocation);
+    void update(const Vec2& touchLocation); /* update method is invoked every frame by the game scene */
 };
